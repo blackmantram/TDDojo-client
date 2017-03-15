2b91d62cc048799a3381da33db90b94b5a672531
blackmantram <misantropee@gmail.com>
2017-03-15 00:35:18 -0500

    added login and register components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,29 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {UIRouterModule} from 'ui-router-ng2';
-import {STATES, MyUIRouterConfig} from './routes.config';
+import {STATES, OTHERWISE} from './routes.config';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header.component';
 import {TitleComponent} from './title.component';
 import {FooterComponent} from './footer.component';
+import {WelcomeComponent} from './welcome.component';
+import {LoginComponent} from './login.component';
+import {RegisterComponent} from './register.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    UIRouterModule.forRoot({states: STATES, otherwise: MyUIRouterConfig.otherwise()})
+    UIRouterModule.forRoot({states: STATES, otherwise: OTHERWISE})
   ],
   declarations: [
     AppComponent,
     HeaderComponent,
     TitleComponent,
-    FooterComponent
+    FooterComponent,
+    WelcomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   bootstrap: [AppComponent]
 })
--- src/app/routes.config.ts
+++ src/app/routes.config.ts
@@ -1,19 +1,14 @@
 import {Injectable} from '@angular/core';
 import {UIRouter, Ng2StateDeclaration} from 'ui-router-ng2';
 
-import {AppComponent} from './app.component';
+import {LoginComponent} from './login.component';
+import {RegisterComponent} from './register.component';
+import {WelcomeComponent} from './welcome.component';
 
 export const STATES: Ng2StateDeclaration[] = [
-  {
-    name: 'App',
-    url: '/',
-    component: AppComponent
-  }
+  { name: 'login', url: '/login', component: LoginComponent },
+  { name: 'register', url: '/register', component: RegisterComponent },
+  { name: 'welcome', url: '/', component: WelcomeComponent }
 ];
 
-@Injectable()
-export class MyUIRouterConfig {
-  static otherwise() {
-    return 'App';
-  }
-}
+export const OTHERWISE = '/';
