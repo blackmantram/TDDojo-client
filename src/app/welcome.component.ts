debfba2c886e57434ec2d48d58f1ccc90d8785fb
blackmantram <misantropee@gmail.com>
2017-03-22 12:55:15 -0500

    restored accidentally deleted component

---
+++ src/app/welcome.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'welcome',
+  template: `
+    <main class="main">
+      <app-title></app-title>
+    </main>
+  `
+})
+export class WelcomeComponent {}
